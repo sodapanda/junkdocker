5e4d1323b1d3603b48b36c1c6dfc7f44742eca6a
sodapanda <sodapanda20@gmail.com>
2020-11-10 22:38:16 +0800

    [add]添加流量统计

--- portforward/entrypoint.sh
+++ portforward/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 echo "start gost portforward"
 DST=$1
-echo ${DST}
+ID=$2
+CID="${ID}_${DST}"
+touch cid
+printf ${CID} >> cid
 tc qdisc add dev eth0 root tbf rate 8mbit latency 1ms burst 1048576
-./gost -L=tcp://:8800/${DST}
+nohup ./gost -L=tcp://:8800/${DST} &
+./portmonitor
