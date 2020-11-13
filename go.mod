e84d8a7afa08d64e4d1eeccd1baa64721eed6b52
sodapanda <sodapanda20@gmail.com>
2020-11-13 13:49:54 +0800

    [add]每个container的流量记录数据库

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/fsouza/go-dockerclient v1.6.6
+	github.com/mattn/go-sqlite3 v1.14.4
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 
 	docker "github.com/fsouza/go-dockerclient"
 )
 
+var db *sql.DB
+
 func main() {
+	var err error
+	db, err = sql.Open("sqlite3", "./junk.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	httpServer()
 }
 
@@ -18,7 +33,15 @@ func httpServer() {
 
 //ReportBandWidth 带宽使用记录
 func ReportBandWidth(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r)
+	cid := getQueryParam(r, "cid")
+	txBytesStr := getQueryParam(r, "txbytes")
+	timeStamp := time.Now().Unix()
+	txBytes, err := strconv.Atoi(txBytesStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	writeDBBandwidth(cid, timeStamp, txBytes)
 }
 
 func testDocker() {
@@ -38,5 +61,19 @@ func testDocker() {
 		fmt.Println("VirtualSize: ", img.VirtualSize)
 		fmt.Println("ParentId: ", img.ParentID)
 		fmt.Println("====================================")
+	}
+}
+
+func getQueryParam(r *http.Request, queryKey string) string {
+	return r.URL.Query()[queryKey][0]
+}
+
+func writeDBBandwidth(containerID string, timeStamp int64, txBytes int) {
+	sqlStmt := fmt.Sprintf("insert into bandwidth(ContainerID,Timestamp,TxBytes) values('%s',%d,%d)",
+		containerID, timeStamp, txBytes)
+	fmt.Println(sqlStmt)
+	_, err := db.Exec(sqlStmt)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
